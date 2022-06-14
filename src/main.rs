b7ec65f5891ef185156c0a93c4c2f1e0d5539336
avdb <avdb@keemail.me>
2022-06-14 23:44:59 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use axum::{
+    routing::{get},
+    // http::StatusCode,
+    Router,
+};
+
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(root));
+
+    let addr = SocketAddr::from(([127,0,0,1], 300));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "test"
+}
